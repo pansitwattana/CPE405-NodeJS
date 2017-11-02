937aad9d48c965b88dd68d58b887c20fd5cf84d4
pansitwattana <pansit.wt@gmail.com>
2017-11-02 14:33:49 +0700

    add post user, query user by id

--- Week 1/expressjs/index.js
+++ Week 1/expressjs/index.js
@@ -1,7 +1,10 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
-var { users } = require('./users')
+var MongoClient = require('mongodb').MongoClient
+var url = 'mongodb://admin:admin@ds243285.mlab.com:43285/customer_lab'
+// var { users } = require('./users')
+let users = []
 
 var app = express()
 
@@ -15,19 +18,23 @@ app.set('views', path.join(__dirname, 'views'));
 //javascript eg. vuejs, angularjs
 
 app.get('/', (req, res) => {
-    //open in public path
+    MongoClient.connect(url)
+        .then((db) => {
+            return db.collection('users').find().toArray()
+        })
+        .then((usersData) => {
+            // return items
+            users = usersData
+            res.render('index', {
+                title: 'Customer List:',
+                users: usersData
+            })
+        })
 })
 
-app.get('/ejs', (req, res) => {
-    res.render('index', {
-        title: 'Customer List:',
-        users: users
-    })
-})
-
-app.get('/user', (req, res) => {
-    res.json(users)
-})
+// app.get('/user', (req, res) => {
+//     res.json(users)
+// })
 
 app.get('/hey', (req, res) => {
     res.send('Hello Wolrd')
@@ -48,8 +55,66 @@ app.post('/user/add', (req, res) => {
         title: 'Customer List',
         users: users
     })
-
 })
+
+app.get('/user/:id', (req, res) => {
+    let id = req.params.id
+
+    MongoClient.connect(url)
+        .then((db) => {
+            return db.collection('users').find({ id: 3 }).toArray()
+        })
+        .then((data) => {
+            console.log(data)
+            if (data.length > 0) {
+                let user = data[0]
+                console.log(user)
+                res.render('user', {
+                    id,
+                    age: user.age,
+                    email: user.email
+                })
+            }
+            else {
+                res.render('user', {
+                    id
+                })
+            }
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    
+    
+})
+
+app.post('/user', (req, res) => {
+    // console.log('rec')
+    let newUser = {
+        name: req.body.name,
+        age: parseInt(req.body.age),
+        email: req.body.email,
+    }
+    // console.log(newUser)
+    MongoClient.connect(url)
+        .then((db) => {
+            return db.collection('users').insertOne(newUser)
+        })
+        .then(() => {
+            // console.log(res)
+            users.push(newUser)
+            res.render('index', {
+                title: 'Customer List',
+                users: users
+            })
+        })
+        .catch((err) => {
+            return err
+        })
+
+    // users.push(newUser)
+})
+
 
 app.listen(3000, () => {
     console.log('Start Server')
